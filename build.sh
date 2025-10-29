fe3528286919fc3fd5d59999219060a28bc3edda
Reginaldo Silva <reginaldo.silva@mindlabs.com.br>
2025-10-28 21:50:43 -0300

    Feat: Add robust command to create default admin on deploy

--- build.sh
+++ build.sh
@@ -2,14 +2,8 @@
 # exit on error
 set -o errexit
 
-# Instala as dependências
 pip install -r requirements.txt
 
-# Coleta os arquivos estáticos (CSS, JS, etc.)
 python manage.py collectstatic --no-input
-
-# Aplica as migrações do banco de dados
 python manage.py migrate
-
-# Cria o superusuário usando nosso comando personalizado
-python manage.py create_superuser
+python manage.py create_default_admin  
