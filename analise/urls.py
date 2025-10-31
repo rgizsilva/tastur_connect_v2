eb9207b1846bd091dc0304c556f825741519c89f
Reginaldo Silva <reginaldo.silva@mindlabs.com.br>
2025-10-30 22:18:42 -0300

    fix: Corrige o namespace da URL do app de análise

--- analise/urls.py
+++ analise/urls.py
@@ -1,15 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AnaliseDadosViewSet, relatorio_analise
 
-# ADICIONE ESTA LINHA
 app_name = 'analise'
 
 router = DefaultRouter()
-router.register(r'dados', AnaliseDadosViewSet)
+# MODIFIQUE ESTA LINHA PARA ADICIONAR O 'basename'
+router.register(r'dados', AnaliseDadosViewSet, basename='analisedados')
 
 urlpatterns = [
     path('relatorio/', relatorio_analise, name='relatorio'),
+    path('', include(router.urls)),
 ]
 
 urlpatterns += router.urls
